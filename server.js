ceb1b43fd130310e30250124e5ff0b10e1ba107e
Riley Shopmyer <rileyshopmyer@gmail.com>
2022-12-04 19:33:29 -0500

    A04 Attempt 1

---
+++ server.js
@@ -0,0 +1,33 @@
+//Server file
+const express = require('express')
+const app = express()
+//const morgan = require('morgan');
+//const db = require('bettersqllite3');
+
+const port = 5000
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//const accesslog = fs.createWriteStream(logdir+'access.log', { flags: 'a' });
+
+//app.use(morgan('', { stream: accesslog }));
+//app.use(db);
+
+app.get('/', (req, res, next) => {
+  res.send('Hello, world!')
+})
+
+app.post('/roll/', (req, res, next) => {
+  console.log("pain")
+})
+
+app.post('/roll/:sides/', (req, res, next) => {
+
+})
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port)
+})
+
+
+
